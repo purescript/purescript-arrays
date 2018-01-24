ad8a27d0b1f726a7c4101de5dad5a359b8f3bb1e
Matthew Leon <ml@matthewleon.com>
2018-01-23 20:59:43 -0500

    use ordering for nubBy
    The old `nubBy` becomes `nubByEq`, which only requires an equality
    relation.

--- test/Test/Data/Array.purs
+++ test/Test/Data/Array.purs
@@ -323,8 +323,11 @@ testArray = do
   assert $ A.nubEq [1, 3, 4, 2, 2, 1] == [1, 3, 4, 2]
 
   log "nubBy should remove duplicate items from the list using a supplied predicate"
+  assert $ A.nubBy compare [1, 3, 4, 2, 2, 1] == [1, 3, 4, 2]
+
+  log "nubByEq should remove duplicate items from the list using a supplied predicate"
   let nubPred = \x y -> if odd x then false else x == y
-  assert $ A.nubBy nubPred [1, 2, 2, 3, 3, 4, 4, 1] == [1, 2, 3, 3, 4, 1]
+  assert $ A.nubByEq nubPred [1, 2, 2, 3, 3, 4, 4, 1] == [1, 2, 3, 3, 4, 1]
 
   log "union should produce the union of two arrays"
   assert $ A.union [1, 2, 3] [2, 3, 4] == [1, 2, 3, 4]
