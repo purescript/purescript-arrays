b26d4dbeb71ac27813a7a57751fd42fd5ffef5e8
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-31 20:58:35 -0800

    Benchmark nubBy

--- bench/Data/Array.purs
+++ bench/Data/Array.purs
@@ -3,6 +3,7 @@ module Bench.Data.Array where
 import Prelude
 
 import Data.Array as Array
+import Data.Traversable (sequence_)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
@@ -10,13 +11,17 @@ import Performance.Minibench (benchWith)
 
 benchArray :: Effect Unit
 benchArray = do
-  log "mapMaybe"
-  log "---------------"
-  benchMapMaybe
+  sequence_ $ Array.intersperse (log "")
+    [ benchMapMaybe
+    , benchNubBy
+    ]
+
 
   where
 
   benchMapMaybe = do
+    log "mapMaybe"
+    log "---------------"
     let shortNats = Array.range 0 100
         longNats = Array.range 0 10000
         onlyEven x = if x `mod` 2 == 0 then Just x else Nothing
@@ -26,3 +31,16 @@ benchArray = do
 
     log $ "mapMaybe (" <> show (Array.length longNats) <> ")"
     benchWith 100 \_ -> Array.mapMaybe onlyEven longNats
+
+  benchNubBy = do
+    log "nubBy"
+    log "---------------"
+    let shortNats = Array.range 0 100
+        longNats = Array.range 0 10000
+        mod3Cmp x y = compare (x `mod` 3) (y `mod` 3)
+
+    log $ "nubBy (" <> show (Array.length shortNats) <> ")"
+    benchWith 1000 \_ -> Array.nubBy mod3Cmp shortNats
+
+    log $ "nubBy (" <> show (Array.length longNats) <> ")"
+    benchWith 100 \_ -> Array.nubBy mod3Cmp longNats
