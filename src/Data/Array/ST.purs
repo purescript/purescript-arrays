527c2eb30fff43e50725804dbf141a8db7284796
Mark Eibes <mark.eibes@gmail.com>
2023-06-16 21:29:48 +0200

    Update ST.purs

--- src/Data/Array/ST.purs
+++ src/Data/Array/ST.purs
@@ -181,7 +181,10 @@ foreign import popImpl
 
 -- | Append an element to the end of a mutable array. Returns the new length of
 -- | the array.
-foreign import push :: forall h a. a -> STArray h a -> ST h Int
+push :: forall h a. a -> (STArray h a) -> ST h Int
+push = runSTFn2 pushImpl
+
+foreign import pushImpl :: forall h a. STFn2 a (STArray h a) h Int
 
 -- | Append the values in an immutable array to the end of a mutable array.
 -- | Returns the new length of the mutable array.
